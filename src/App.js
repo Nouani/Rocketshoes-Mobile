b395e5dc543f06dec31f5c95b41f9b17b2cf95c9
Nouani <nouanigabriel@hotmail.com>
2020-02-16 20:45:10 -0300

    Iniciando Projeto

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {} from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  return <Text>Hello World</Text>;
+}
