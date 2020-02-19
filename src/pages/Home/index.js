792c834256277b27e9a0af015ca2835af78638a5
Nouani <nouanigabriel@hotmail.com>
2020-02-18 22:07:56 -0300

    Configuração do Axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 import { Container } from './styles';
 
-export default function Home() {
-    return <Text>HOME</Text>;
+import api from '../../services/api';
+
+export default class Home extends Component {
+    async componentDidMount() {
+        const response = await api.get('/products');
+        console.log(response);
+    }
+
+    render() {
+        return <Text>HOME</Text>;
+    }
 }
