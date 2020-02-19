821182bd2ff2f36386ef257a5a5d410241058312
Nouani <nouanigabriel@hotmail.com>
2020-02-19 13:42:48 -0300

    Listando produtos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Lista, ContainerProduct, ImageProduct, Name, Price, ButtonContainer, AddProduct, ProductAmount, Amount } from './styles';
 
-export default function Home() {
-    return <Text>HOME</Text>;
+import api from '../../services/api';
+
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
+
+    async componentDidMount() {
+        // const response = await api.get('/cep/13070770');
+        const response = await api.get('/products');
+        this.setState({ products: response.data });
+    }
+
+    handleAddProduct = ({ item }) => {
+        return (
+            <ContainerProduct>
+                <ImageProduct source={{ uri: item.image }} />
+                <Name>{item.title}</Name>
+                <Price>{item.price}</Price>
+                <ButtonContainer>
+                    <ProductAmount>
+                        <Icon name="add-shopping-cart" color="#FFF" size={20} />
+                        <Amount>0</Amount>
+                    </ProductAmount>
+                    <AddProduct>Adicionar</AddProduct>
+                </ButtonContainer>
+            </ContainerProduct>
+        )
+    }
+
+    render() {
+        const { products } = this.state;
+        return (
+            <Lista
+                data={products}
+                horizontal
+                keyExtractor={product => String(product.id)}
+                renderItem={this.handleAddProduct}
+            />
+        );
+    }
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,53 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Lista = styled.FlatList`
+    margin: 10px 15px;
+`;
+
+export const ContainerProduct = styled.View`
+    padding: 8px;
+    margin-right: 16px;
+    width: 180px;
+    height: 300px;
+    background: #FFF;
+    border-radius: 8px;
+`;
+
+export const ImageProduct = styled.Image`
+    width: 160px;
+    height: 160px;
+    align-self: center;
+`;
+
+export const Name = styled.Text`
+    font-size: 12px;
+`;
+
+export const Price = styled.Text`
+    font-weight: bold;
+    font-size: 16px;
+    margin-top: 5px;
+`;
+
+export const ButtonContainer = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+    background: #7159c1;
+    border-radius: 2px;
+    height: 35px;
+    margin-top: auto;
+`;
+
+export const AddProduct = styled.Text`
+    color: #FFF;
+    font-weight: bold;
+    margin-left: 10px;
+`;
+export const ProductAmount = styled.Text`
+    margin: 0 5px;
+    align-items: center;
+`;
+export const Amount = styled.Text`
+    color: #FFF;
+`;
+
