c871f3881cd12a6da0d04819dab42519b734b720
Nouani <nouanigabriel@hotmail.com>
2020-03-15 14:43:17 -0300

    Configuração do Reactotron

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {} from 'react-native';
 import { Provider } from 'react-redux';
 
+import './config/reactotronConfig';
+
 import Routes from './routes';
 import store from './store';
 
