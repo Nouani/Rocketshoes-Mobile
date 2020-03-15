faa71484a19d01287679a8ce8ec07781c40d9d52
Nouani <nouanigabriel@hotmail.com>
2020-03-15 12:42:06 -0300

    Estrutura do Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import {
+    Container,
+    Product,
+    ProductHeader,
+    ProductImage,
+    Details,
+    ProductTitle,
+    ProductPrice,
+    ProductDelete,
+    ProductFooter,
+    ProductControls,
+    ProductControlButton,
+    ProductAmount,
+    ProductSubtotal,
+    Footer,
+    TotalText,
+    TotalPrice,
+    Order,
+    OrderText,
+} from './styles';
 
-export default function Cart() {
-    return <Text>CART</Text>;
+import colors from '../../styles/colors';
+
+export default class Cart extends Component {
+    state = {};
+
+    render() {
+        return (
+            <Container>
+                <Product>
+                    <ProductHeader>
+                        <ProductImage
+                            source={{
+                                uri:
+                                    'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+                            }}
+                        />
+                        <Details>
+                            <ProductTitle>
+                                Tênis de Caminhada Leve Confortável
+                            </ProductTitle>
+                            <ProductPrice>R$179,90</ProductPrice>
+                        </Details>
+                        <ProductDelete>
+                            <Icon
+                                name="delete-forever"
+                                size={24}
+                                color={colors.primary}
+                            />
+                        </ProductDelete>
+                    </ProductHeader>
+                    <ProductFooter>
+                        <ProductControls>
+                            <ProductControlButton>
+                                <Icon
+                                    name="remove-circle-outline"
+                                    size={20}
+                                    color={colors.primary}
+                                />
+                            </ProductControlButton>
+                            <ProductAmount value={String(1)} />
+                            <ProductControlButton>
+                                <Icon
+                                    name="add-circle-outline"
+                                    size={20}
+                                    color={colors.primary}
+                                />
+                            </ProductControlButton>
+                        </ProductControls>
+                        <ProductSubtotal>R$179,90</ProductSubtotal>
+                    </ProductFooter>
+                </Product>
+                <Footer>
+                    <TotalText>Total</TotalText>
+                    <TotalPrice>R$179,90</TotalPrice>
+                    <Order>
+                        <OrderText>Finalizar Pedido</OrderText>
+                    </Order>
+                </Footer>
+            </Container>
+        );
+    }
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,3 +1,63 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    flex-direction: column;
+    background: #fff;
+    margin: 10px 18px;
+    border-radius: 8px;
+    padding: 15px 10px;
+`;
+
+export const Product = styled.View`
+    border: 1px solid red;
+    flex-direction: column;
+`;
+
+export const ProductHeader = styled.View`
+    border: 1px solid red;
+    flex-direction: row;
+`;
+
+export const ProductImage = styled.Image`
+    width: 30px;
+    height: 30px;
+`;
+
+export const Details = styled.View``;
+
+export const ProductTitle = styled.Text``;
+
+export const ProductPrice = styled.Text``;
+
+export const ProductDelete = styled.TouchableOpacity``;
+
+export const ProductFooter = styled.View`
+    border: 1px solid red;
+    flex-direction: row;
+`;
+
+export const ProductControls = styled.View`
+    border: 1px solid red;
+    flex-direction: row;
+`;
+
+export const ProductControlButton = styled.TouchableOpacity``;
+
+export const ProductAmount = styled.TextInput.attrs({
+    readonly: true,
+})``;
+
+export const ProductSubtotal = styled.Text``;
+
+export const Footer = styled.View`
+    border: 1px solid red;
+    flex-direction: column;
+`;
+
+export const TotalText = styled.Text``;
+
+export const TotalPrice = styled.Text``;
+
+export const Order = styled.TouchableOpacity``;
+
+export const OrderText = styled.Text``;
