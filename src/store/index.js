ddb0a3ee74f338f2ec67042c603ab047c31021d8
Nouani <nouanigabriel@hotmail.com>
2020-03-18 18:19:33 -0300

    Configuração do Redux-Saga com Reactotron

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,10 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
+const sagaMiddleware = createSagaMiddleware({
+    sagaMonitor,
+});
 
 const enhancer = __DEV__
     ? compose(console.tron.createEnhancer(), applyMiddleware(sagaMiddleware))
