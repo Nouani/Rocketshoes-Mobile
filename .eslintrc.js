a208093b4763da69028f7c48688f8c29f78f833c
Nouani <nouanigabriel@hotmail.com>
2020-03-18 14:10:45 -0300

    Produto duplicado

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,5 +34,6 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
     'no-console': ["error", { allow: ["tron"] }],
+    'no-param-reassign': 'off',
   },
 };
