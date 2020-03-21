2d726037097c2e282514cda28852f2cf6397d094
Nouani <nouanigabriel@hotmail.com>
2020-03-21 00:10:17 -0300

    Carrinho vazio

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -42,67 +42,76 @@ function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
 
     return (
         <Container>
-            {cart.map(product => (
-                <Product key={product.id}>
-                    <ProductHeader>
-                        <ProductImage
-                            source={{
-                                uri: product.image,
-                            }}
-                        />
-                        <Details>
-                            <ProductTitle>{product.title}</ProductTitle>
-                            <ProductPrice>
-                                {product.priceFormatted}
-                            </ProductPrice>
-                        </Details>
-                        <ProductDelete
-                            onPress={() => removeFromCart(product.id)}
-                        >
-                            <Icon
-                                name="delete-forever"
-                                size={24}
-                                color={colors.primary}
-                            />
-                        </ProductDelete>
-                    </ProductHeader>
-                    <ProductFooter>
-                        <ProductControls>
-                            <ProductControlButton
-                                onPress={() => decrement(product)}
-                            >
-                                <Icon
-                                    name="remove-circle-outline"
-                                    size={20}
-                                    color={colors.primary}
+            {cart.length > 0 ? (
+                <>
+                    {cart.map(product => (
+                        <Product key={product.id}>
+                            <ProductHeader>
+                                <ProductImage
+                                    source={{
+                                        uri: product.image,
+                                    }}
                                 />
-                            </ProductControlButton>
-                            <ProductAmount value={String(product.amount)} />
-                            <ProductControlButton
-                                onPress={() => increment(product)}
-                            >
-                                <Icon
-                                    name="add-circle-outline"
-                                    size={20}
-                                    color={colors.primary}
-                                />
-                            </ProductControlButton>
-                        </ProductControls>
-                        <ProductSubtotal>{product.subtotal}</ProductSubtotal>
-                    </ProductFooter>
-                </Product>
-            ))}
-            <Footer>
-                <TotalText>TOTAL</TotalText>
-                <TotalPrice>{total}</TotalPrice>
-                <Order>
-                    <OrderText>FINALIZAR PEDIDO</OrderText>
-                </Order>
-            </Footer>
-            {/* <EmptyContainer>
+                                <Details>
+                                    <ProductTitle>{product.title}</ProductTitle>
+                                    <ProductPrice>
+                                        {product.priceFormatted}
+                                    </ProductPrice>
+                                </Details>
+                                <ProductDelete
+                                    onPress={() => removeFromCart(product.id)}
+                                >
+                                    <Icon
+                                        name="delete-forever"
+                                        size={24}
+                                        color={colors.primary}
+                                    />
+                                </ProductDelete>
+                            </ProductHeader>
+                            <ProductFooter>
+                                <ProductControls>
+                                    <ProductControlButton
+                                        onPress={() => decrement(product)}
+                                    >
+                                        <Icon
+                                            name="remove-circle-outline"
+                                            size={20}
+                                            color={colors.primary}
+                                        />
+                                    </ProductControlButton>
+                                    <ProductAmount
+                                        value={String(product.amount)}
+                                    />
+                                    <ProductControlButton
+                                        onPress={() => increment(product)}
+                                    >
+                                        <Icon
+                                            name="add-circle-outline"
+                                            size={20}
+                                            color={colors.primary}
+                                        />
+                                    </ProductControlButton>
+                                </ProductControls>
+                                <ProductSubtotal>
+                                    {product.subtotal}
+                                </ProductSubtotal>
+                            </ProductFooter>
+                        </Product>
+                    ))}
+                    <Footer>
+                        <TotalText>TOTAL</TotalText>
+                        <TotalPrice>{total}</TotalPrice>
+                        <Order>
+                            <OrderText>FINALIZAR PEDIDO</OrderText>
+                        </Order>
+                    </Footer>
+                </>
+            ) : (
+                <EmptyContainer>
                     <Icon name="remove-shopping-cart" size={64} color="#eee" />
                     <EmptyText>Seu carrinho está vazio.</EmptyText>
-                </EmptyContainer> */}
+                </EmptyContainer>
+            )}
         </Container>
     );
 }
